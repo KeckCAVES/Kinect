ce120d6b7e47bd8601d10cefe7220a2500d58a0d
Oliver Kreylos <kreylos@cs.ucdavis.edu>
2010-12-02 14:32:57 -0800

    Release version 1.2 revision 2
    Using tarball (with SHA1):
    2bbe217cd503743f967467052de97a341c6b2c51 *Kinect-1.2.tar.gz

--- KinectCamera.cpp
+++ KinectCamera.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 KinectCamera - Wrapper class to represent the color and depth camera
 interface aspects of the Kinect sensor.
-Copyright (c) 2010-2011 Oliver Kreylos
+Copyright (c) 2010 Oliver Kreylos
 
 This file is part of the Kinect 3D Video Capture Project (Kinect).
 
@@ -29,7 +29,8 @@ Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 #include <iostream>
 #include <Misc/ThrowStdErr.h>
 #include <Misc/FunctionCalls.h>
-#include <IO/ValueSource.h>
+#include <Misc/FileCharacterSource.h>
+#include <Misc/ValueSource.h>
 
 #include "USBContext.h"
 #include "USBDeviceList.h"
@@ -60,7 +61,7 @@ void initHexDigits(void)
 		}
 	}
 
-inline unsigned int readUInt(IO::ValueSource& source)
+inline unsigned int readUInt(Misc::ValueSource& source)
 	{
 	unsigned int result=0x0U;
 	
@@ -75,7 +76,7 @@ inline unsigned int readUInt(IO::ValueSource& source)
 	return result;
 	}
 
-inline size_t readUCharBuffer(IO::ValueSource& source,unsigned char* buffer,size_t bufferSize)
+inline size_t readUCharBuffer(Misc::ValueSource& source,unsigned char* buffer,size_t bufferSize)
 	{
 	size_t result=0;
 	
@@ -603,7 +604,7 @@ void KinectCamera::startStreaming(KinectCamera::StreamingCallback* newColorStrea
 	/* Open and prepare the device: */
 	open();
 	// setConfiguration(1); // This seems to confuse the device
-	claimInterface(0,true);
+	claimInterface(0);
 	
 	/* Check if caller wants to receive color frames: */
 	if(newColorStreamingCallback!=0)
